8c8e3aab6cdefff28882a95ee3f592d7c7e53b14
钟颖 <cyan.zy@alibaba-inc.com>
2016-10-10 19:02:57 +0800

    Add help links

--- xTextHandler/AppViewController.swift
+++ xTextHandler/AppViewController.swift
@@ -16,8 +16,27 @@
 import Cocoa
 
 class AppViewController: NSViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    func open(_ string: String) {
+        if let url = URL(string: string) {
+            NSWorkspace.shared().open(url)
+        }
+    }
+    
+    @IBAction func usage(_ sender: AnyObject) {
+        open("https://github.com/cyanzhong/xTextHandler/")
+    }
+    
+    @IBAction func basic(_ sender: AnyObject) {
+        open("https://developer.apple.com/xcode/")
+    }
+    
+    @IBAction func session(_ sender: AnyObject) {
+        open("https://developer.apple.com/videos/play/wwdc2016/414/")
+    }
+    
+    @IBAction func github(_ sender: AnyObject) {
+        open("https://github.com/cyanzhong/")
     }
 }
 
